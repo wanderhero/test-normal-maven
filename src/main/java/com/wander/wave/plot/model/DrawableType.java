7dab9bbb74db5f26686d407b1eaafef6b9c7c062
wanderhero <cengjingsea@163.com>
2017-05-19 17:46:26 +0800

    处理wav文件

---
+++ src/main/java/com/wander/wave/plot/model/DrawableType.java
@@ -0,0 +1,17 @@
+package com.wander.wave.plot.model;
+
+/**
+ * 可绘制对象类型
+ * @author RobinTang
+ * 
+ */
+public enum DrawableType {
+
+	Point, // o	点
+	Line, // -	线
+	Mark, // *	标记
+	Columnar, // n	柱状
+	MarkLine, // $	点线
+	Step, // z 梯度
+
+}
