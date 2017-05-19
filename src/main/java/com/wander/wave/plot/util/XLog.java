43ee0ce73fbcb28ce546a0f8d1be6d4a39e30c07
wanderhero <cengjingsea@163.com>
2017-05-19 17:48:17 +0800

    处理wav文件

---
+++ src/main/java/com/wander/wave/plot/util/XLog.java
@@ -0,0 +1,13 @@
+package com.wander.wave.plot.util;
+
+final public class XLog {
+
+	public static void log(String log) {
+		System.out.println(log);
+	}
+
+	public static void log(String format, Object... args) {
+		log(String.format(format, args));
+	}
+
+}
